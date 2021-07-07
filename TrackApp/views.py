ce6c4853a4fb3ce33787f7d11333bec7e3425bf7
alguerre <alguerre@outlook.com>
2021-07-07 18:11:49 +0200

    Register #2

--- TrackApp/views.py
+++ TrackApp/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+
+from .models import User
 
 
 def index(request):
@@ -9,7 +12,30 @@ def index(request):
 
 
 def register(request):
-    return HttpResponse('Register')
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "TrackApp/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "TrackApp/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "TrackApp/register.html")
 
 
 def login(request):
