c2baae326150253cb751403f20e0363505f355af
alguerre <alguerre@outlook.com>
2021-07-09 22:56:37 +0200

    Migrate constants needed in track.py

--- TrackApp/constants.py
+++ TrackApp/constants.py
@@ -9,6 +9,12 @@ import datetime
 maximum_file_size = 10e+6
 default_datetime = datetime.datetime(2000, 1, 1, 0, 0, 0)
 
+# fix elevation
+steep_distance = 0.2  # steep zone is always longer than X m
+steep_gap = 0.6  # threshold to consider a steep zone in elevation
+steep_k_moving_average = 20  # step for moving average if needed
+fix_thr = 1000  # under 1000 points smoothing is used instead of fixing
+
 # OSM request options
 version = "v0.0"
 email = "alguerre@outlook.com"
