4c2c189667b819d327ed2b1e00229763751a6b0f
alguerre <alguerre@outlook.com>
2021-07-24 01:12:24 +0200

    Modify track names management and include basics scheme for remove/change tracks #27

--- TrackApp/static/TrackApp/editor.js
+++ TrackApp/static/TrackApp/editor.js
@@ -6,24 +6,55 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function submit_file() {
-    document.querySelector("#select-file").onchange = function() {
-        document.querySelector("form").submit();
+    document.querySelector('#select-file').onchange = function() {
+        document.querySelector('form').submit();
         document.querySelector('#div_spinner').style.display = 'inline-block';
     };
 }
 
 
 function insert_track_name() {
-    let div_track_list = document.querySelector("#div_track_list");
+    let div_track_list = document.querySelector('#div_track_list');
     let track_list = eval(div_track_list.dataset.track_list);
 
-    if (typeof track_list !== "undefined") {
+    if (typeof track_list !== 'undefined') {
         for (var i = 0; i < track_list.length; i++) {
             let color = get_color(i, alpha='-1');
-            console.log(i, color);
-            let identifier = `<span style="font-size: 20px; color: transparent;  text-shadow: 0 0 0 ${color};">&#9899;</span>`;
-            console.log(identifier);
-            div_track_list.innerHTML += `<p>${identifier} <span contenteditable style="font-size: 18px;">${track_list[i]}</span></p>`;
+
+            const p_name = document.createElement('p');
+            const span_name = document.createElement('span');
+            const span_marker = document.createElement('span');
+            const button_remove = document.createElement('button');
+
+            span_marker.innerHTML = '&#9899';
+            span_marker.style = `font-size: 20px; color: transparent;  text-shadow: 0 0 0 ${color};`;
+
+            span_name.style = 'font-size: 18px; margin-left: 5px;';
+            span_name.setAttribute('contenteditable', 'true');
+            span_name.innerHTML = track_list[i];
+            span_name.addEventListener('blur', function() {
+                console.log('Change track name ', i, span_name.innerHTML);
+                // TODO create fetch for change_segment_name endpoint
+                // TODO store in track a name for each segment
+                // TODO convert track segment to JSON
+            });
+
+            button_remove.setAttribute('class', 'btn-close');
+            button_remove.setAttribute('type', 'button');
+            button_remove.setAttribute('aria-label', 'Close');
+            button_remove.style = 'font-size: 18px; vertical-align: -3px; margin-left: 20px;';
+            button_remove.addEventListener('click', function() {
+                console.log('Remove track ', i);
+                p_name.style.display = 'none';
+                // TODO remove segment from pandas
+                // TODO remove segment plot
+                // TODO create fetch for remove_segment endpoint
+            });
+
+            p_name.appendChild(span_marker);
+            p_name.appendChild(span_name);
+            p_name.appendChild(button_remove);
+            div_track_list.appendChild(p_name);
         }
     }
 }
@@ -69,6 +100,6 @@ function insert_map() {
                 source: new ol.source.OSM()
             }),
         ],
-        target: "js-map"
+        target: 'js-map'
     });
 }
