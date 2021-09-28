004c233a52b36e10c53eb36e9b93bd59d9b16d12
alguerre <alguerre@outlook.com>
2021-09-28 23:28:59 +0200

    Use plot.js

--- editor/static/editor/editor.js
+++ editor/static/editor/editor.js
@@ -1,4 +1,5 @@
 import * as utils from "./utils.js";
+import * as plot from "./plot.js";
 let selected_segments = 0;
 let selected_segment_idx;
 let track;
@@ -8,8 +9,8 @@ let chart;
 
 document.addEventListener('DOMContentLoaded', async () => {
     track = await load_track();
-    map = create_map();
-    chart = create_chart();  // elevation chart
+    map = plot.create_map();
+    chart = plot.create_chart();  // elevation chart
     submit_file();
     plot_track();
     segments_manager();
@@ -60,7 +61,7 @@ function segments_manager() {
 
 
 function manage_segment(segment) {
-    let color = get_color(segment.index, '-1');
+    let color = plot.get_color(segment.index, '-1');
     const p_name = document.createElement('p');
     const span_name = document.createElement('span');
     const span_marker = document.createElement('span');
@@ -347,60 +348,6 @@ function relink_elevation(segment_index) {
 }
 
 
-function get_color(color_index, alpha='0.5') {
-    /*
-    GET_COLOR returns the rgb string corresponding to a number of predefined
-    colors
-    */
-    if (color_index < 0) {
-        color_index = 0;
-    }
-    else {
-        color_index = color_index - 1;  // indexing from 1
-    }
-
-    const colors = ['255, 127, 80',  // coral
-                    '30, 144, 255',  // dodgerblue
-                    '50, 205, 50', // limegreen
-                    '255, 105, 180', // hotpink
-                    '250, 128, 114',  // salmon
-                    '123, 104, 238', // MediumSlateBlue
-                    '34, 139, 34', // forestgreen
-                    '255, 0, 0',  // red
-                    '95, 158, 160',  // cadetblue
-                    '218, 112, 214',  // orchid
-                    '189, 183, 107',  // darkkhaki
-                    '160, 82, 45',  // sienna
-                    '255, 215, 0', // gold
-                    '64, 224, 208',  // turquoise
-                    '0, 128, 128', // teal
-                   ];
-
-    if (alpha === '-1') {
-        return `rgb(${colors[color_index]})`;
-    }
-
-    return `rgb(${colors[color_index]}, ${alpha})`;
-}
-
-
-function create_map() {
-    return new ol.Map({
-        view: new ol.View({
-            center: ol.proj.fromLonLat([0, 0]),
-            zoom: 1,
-            maxZoom: 16,
-            minZoom: 1
-        }),
-        layers: [
-            new ol.layer.Tile({
-                source: new ol.source.OSM()
-            }),
-        ],
-        target: 'js-map'
-    });
-}
-
 function plot_elevation(segment) {
     // Plot elevation
     let elevation_data = [];
@@ -416,8 +363,8 @@ function plot_elevation(segment) {
         data: elevation_data,
         showLine: true,
         borderWidth: 3,
-        backgroundColor: get_color(segment.index, '0.2'),
-        borderColor: get_color(segment.index, '0.8'),
+        backgroundColor: plot.get_color(segment.index, '0.2'),
+        borderColor: plot.get_color(segment.index, '0.8'),
         hidden: false,
     });
     chart.update();
@@ -428,17 +375,17 @@ function plot_segment(segment) {
 
     // Points to vector layer
     const points_vector_layer = new ol.layer.Vector({
-        source: get_points_source(segment.lat, segment.lon),
-        style: get_points_style(segment.index),
+        source: plot.get_points_source(segment.lat, segment.lon),
+        style: plot.get_points_style(segment.index),
         name: `layer_points_${segment.index}`,
     });
     map.addLayer(points_vector_layer);
 
     // Lines to vector layer
     const lines_vector_layer = new ol.layer.Vector({
-        source: get_lines_source(segment.lat, segment.lon,
+        source: plot.get_lines_source(segment.lat, segment.lon,
                                  `features_lines_${segment.index}`,
-                                 get_lines_style(segment.index)),
+                                 plot.get_lines_style(segment.index)),
         name: `layer_lines_${segment.index}`,
     });
     map.addLayer(lines_vector_layer);
@@ -455,7 +402,7 @@ function plot_segment(segment) {
             if (e.selected[0].getId() === `features_lines_${segment.index}`) {
                 e.selected[0].setStyle(new ol.style.Style({
                     stroke: new ol.style.Stroke({
-                        color: get_color(segment.index, '0.9'),
+                        color: plot.get_color(segment.index, '0.9'),
                         width: 7,
                     })
                 }));
@@ -477,113 +424,13 @@ function plot_segment(segment) {
 }
 
 
-function get_points_source(lat, lon) {
-    /*
-    GET_POINTS_SOURCE generates a vector source with points of pairs
-    latitude-longitude
-    */
-
-    // create points
-    const features = [];
-    for (let i = 0; i < lat.length; i++) {
-        features.push(new ol.Feature({
-            geometry: new ol.geom.Point(ol.proj.fromLonLat([
-                lon[i], lat[i]
-            ]))
-        }));
-    }
-
-    // create the source and layer for features
-    return new ol.source.Vector({
-        features
-    });
-}
-
-
-function get_lines_source(lat, lon, id, style) {
-    /*
-    GET_LINES_SOURCE generates a vector source with a line joining pairs
-    of coordinates latitude-longitude
-    */
-
-    // create points
-    const points = [];
-    for (let i = 0; i < lat.length; i++) {
-        points.push(ol.proj.fromLonLat([lon[i], lat[i]]));
-    }
-
-    const featureLine = new ol.Feature({
-        geometry: new ol.geom.LineString(points)
-    });
-    featureLine.setId(id);
-    featureLine.setStyle(style);
-
-    // create the source and layer for features
-    return new ol.source.Vector({
-        features: [featureLine]
-    });
-}
-
-
-function get_points_style(color_index) {
-    /*
-    GET_POINTS_STYLE provides a style for points
-    */
-    return new ol.style.Style({
-        image: new ol.style.Circle({
-            fill: new ol.style.Fill({color: get_color(color_index)}),  // inner color
-            radius: 3,  // circle radius
-        }),
-    });
-}
-
-
-function get_lines_style(color_index, alpha) {
-    /*
-    GET_LINES_STYLE provides a style for lines
-    */
-    return new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: get_color(color_index, alpha),
-            width: 5,
-        })
-    });
-}
-
-
 function plot_link_coor(link) {
     const link_vector_layer = new ol.layer.Vector({
-        source: get_links_source(link['from_coor'], link['to_coor']),
-        style: get_link_style(),
+        source: plot.get_links_source(link['from_coor'], link['to_coor']),
+        style: plot.get_link_style(),
         name: `layer_link_${link['from']}_${link['to']}`,
     });
     map.addLayer(link_vector_layer);
-}
-
-function get_links_source(from, to) {
-    // create points
-    const points = [];
-    points.push(ol.proj.fromLonLat([from['lon'], from['lat']]));
-    points.push(ol.proj.fromLonLat([to['lon'], to['lat']]));
-
-    const featureLink = new ol.Feature({
-        geometry: new ol.geom.LineString(points)
-    });
-
-    // create the source and layer for features
-    return new ol.source.Vector({
-        features: [featureLink]
-    });
-}
-
-
-function get_link_style() {
-    return new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: 'rgb(0, 0, 128, 0.1)',  // navy color
-            width: 3,
-        })
-    });
 }
 
 
@@ -1010,7 +857,7 @@ function change_segments_order() {
 
         Array.prototype.forEach.call(segments, el => {
             if (el.style.display !== 'none'){
-                let color = get_color(el.dataset.segment_idx, '-1');
+                let color = plot.get_color(el.dataset.segment_idx, '-1');
 
                 const div_segment = document.createElement('div');
                 const span_name = document.createElement('span');
@@ -1135,43 +982,6 @@ function change_segments_order() {
 
 }
 
-
-function create_chart() {
-    return new Chart("js-elevation", {
-       type: "scatter",
-       data: {},
-       options: {
-           responsive: true,
-           maintainAspectRatio: false,
-           plugins: {
-               legend: {
-                   display: false
-               },
-               tooltips: {
-                   enabled: false
-               },
-           },
-           scales: {
-               x: {
-                   ticks: {
-                       callback: (value) => {
-                           return value + ' km';
-                       }
-                   }
-               },
-               y: {
-                   ticks: {
-                       callback: (value) => {
-                           return value + ' m';
-                       }
-                   }
-               },
-           }
-       }
-   });
-}
-
-
 function elevation_show_segment(index=undefined, all=false) {
     chart.data.datasets.forEach(dataset => {
         if (all) {
@@ -1201,30 +1011,9 @@ function update_distance() {
         let end = previous_segment.lat.length - 1;
         let distance = current_segment['segment_distance'];
         let seg2seg_distance = previous_segment['distance'][end] +
-                               haversine_distance([current_segment.lat[0], current_segment.lon[0]],
+                               plot.haversine_distance([current_segment.lat[0], current_segment.lon[0]],
                                                   [previous_segment.lat[end], previous_segment.lon[end]]);
         distance = distance.map(a => a +  seg2seg_distance);
         track['segments'][i]['distance'] = distance;
     }
-}
-
-function haversine_distance([lat1, lon1], [lat2, lon2]) {
-    /* Compute distance between two coordinates */
-    const toRadian = angle => (Math.PI / 180) * angle;
-    const distance = (x, y) => (Math.PI / 180) * (x - y);
-    const RADIUS_OF_EARTH_IN_KM = 6371;
-
-    const dLat = distance(lat2, lat1);
-    const dLon = distance(lon2, lon1);
-
-    lat1 = toRadian(lat1);
-    lat2 = toRadian(lat2);
-
-    // Haversine Formula
-    const a =
-        Math.pow(Math.sin(dLat / 2), 2) +
-        Math.pow(Math.sin(dLon / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
-    const c = 2 * Math.asin(Math.sqrt(a));
-
-    return RADIUS_OF_EARTH_IN_KM * c;
 }
