7aaf6519279711da38b39d1a82d6d39eefa2cd35
alguerre <alguerre@outlook.com>
2021-10-17 19:21:09 +0200

    New debug environment variable and allowed hosts

--- TrackEditorWeb/settings.py
+++ TrackEditorWeb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG_VALUE'] == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trackeditor.herokuapp.com']
 
 
 # Application definition
